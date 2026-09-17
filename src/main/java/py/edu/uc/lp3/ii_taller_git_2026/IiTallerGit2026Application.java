66c30161d530f5b62159232665c713128f29a3e3
IrinaInsfran <irina.insfran@gmail.com>
2026-09-17 12:02:18 -0300

    chore: esqueleto Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/ii_taller_git_2026/IiTallerGit2026Application.java
@@ -0,0 +1,12 @@
+package py.edu.uc.lp3.ii_taller_git_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiTallerGit2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IiTallerGit2026Application.class, args);
+    }
+}
